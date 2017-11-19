936092bd1cb67760c2a46f3e7158d9ac314c2715
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2017-11-19 15:48:24 -0800

    Initial Commit

---
+++ SwiftyCoreData_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyCoreData_Demo
+//
+//  Created by Cyril Garcia on 11/19/17.
+//  Copyright © 2017 ByCyril. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
